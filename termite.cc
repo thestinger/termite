2865781c707fdb6bcbfa3ef66e6abbe382cd683b
Daniel Micay <danielmicay@gmail.com>
2012-08-14 23:52:28 -0400

    fix focus loss issue with the completion widget

--- termite.cc
+++ termite.cc
@@ -432,6 +432,9 @@ gboolean entry_key_press_cb(GtkEntry *entry, GdkEventKey *event, search_panel_in
         } else if (event->keyval == GDK_KEY_ISO_Left_Tab) {
             synthesize_keypress(GTK_WIDGET(entry), GDK_KEY_Up);
             return TRUE;
+        } else if (event->keyval == GDK_KEY_Down) {
+            // this stops the down key from leaving the GtkEntry...
+            event->hardware_keycode = 0;
         }
     }
 
