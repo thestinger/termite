ccb10830122a56142b55086ad8d6bb5a2b86b463
Fangrui Song <i@maskray.me>
2018-05-07 21:40:49 -0700

    Use recursive subpattern to match balanced parentheses.
    This skips the trailing ) in [](http://example) while matches
    https://en.wikipedia.org/wiki/Haskell_(programming_language)

--- url_regex.hh
+++ url_regex.hh
@@ -9,7 +9,7 @@
 #define PORT            "(?:\\:[[:digit:]]{1,5})?"
 #define SCHEME          "(?:[[:alpha:]][+-.[:alnum:]]*:)"
 #define USERPASS        USERCHARS_CLASS "+(?:\\:" PASSCHARS_CLASS "+)?"
-#define URLPATH         "(?:/[[:alnum:]\\Q-_.!~*'();/?:@&=+$,#%\\E]*)?"
+#define URLPATH         "((?>[[:alnum:]\\Q-_.!~*';/?:@&=+$,#%\\E]+)|\\((?-1)\\))*"
 
 const char * const url_regex = SCHEME "//(?:" USERPASS "\\@)?" HOST PORT URLPATH;
 
